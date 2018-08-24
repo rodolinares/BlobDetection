fa8c7d47101edd1bf75e1601c52d461c83115326
Rodrigo Linares <rodolinares19@gmail.com>
2018-08-24 00:58:29 -0500

    Initial commit

---
+++ BlobDetection/Program.cs
@@ -0,0 +1,43 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace BlobDetection
+{
+    class Program
+    {
+        private static string _input = "blob.jpg";
+
+        static void Main()
+        {
+            var image = CvInvoke.Imread(_input, ImreadModes.Grayscale);
+
+            var parameters = new SimpleBlobDetectorParams
+            {
+                MinThreshold = 10,
+                MaxThreshold = 200,
+
+                FilterByArea = true,
+                MinArea = 1500,
+
+                FilterByCircularity = true,
+                MinCircularity = 0.1f,
+
+                FilterByConvexity = true,
+                MinConvexity = 0.87f,
+
+                FilterByInertia = true,
+                MinInertiaRatio = 0.01f
+            };
+
+            var detector = new SimpleBlobDetector(parameters);
+            var keypoints = new VectorOfKeyPoint(detector.Detect(image));
+            var output = new Mat();
+            Features2DToolbox.DrawKeypoints(image, keypoints, output, new Bgr(0, 0, 255), Features2DToolbox.KeypointDrawType.DrawRichKeypoints);
+            CvInvoke.Imshow("keypoints", output);
+            CvInvoke.WaitKey();
+        }
+    }
+}
